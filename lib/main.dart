e7cec7ea537850b17afb67d232847250708f97b3
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-03 08:30:42 +0430

    commit> Add Cylinder & Sphere

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutterar/home_page.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String _title = "Flutter AR";
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter AR',
+      title: _title,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter AR'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-      ),
+      home: HomePage(title:_title),
     );
   }
 }
